c7ef8e4915d003e09518f9cbb0e769c8b3275d80
Bhuvi-Bhuvaneshwari <Bhuvaneshwari2023@gmail.com>
2021-08-08 18:33:28 -0400

    bhuvi

---
+++ library/src/main/java/com/app/library/library.java
@@ -0,0 +1,15 @@
+package com.app.library;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class library extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
